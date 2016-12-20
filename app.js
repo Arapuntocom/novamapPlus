ed7682a5b389b0ed31b3a61f8de0e21b700a22be
Aracelly A <Aracelly A>
2016-12-20 19:02:17 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ngRoute',
+  'ui.router', 
+  'header',
+  'draw',
+  'draw.tree',
+  'myApp.version',
+  'dibujo'
+])
+
+.config(['$locationProvider', '$routeProvider', '$stateProvider', '$urlRouterProvider', function($locationProvider, $routeProvider, $stateProvider, $urlRouterProvider) {
+   $locationProvider.hashPrefix('!');
+  $urlRouterProvider.otherwise("/");
+  $stateProvider
+
+  .state('inicio', {
+    url: '/',
+    templateUrl: 'view/add.html' 
+  })
+
+  .state('draw', {
+    url: '/draw',   
+    templateUrl: 'view/draw.html'
+  })
+
+  .state('dibujo', {
+    url: '/dibujo',
+    templateUrl: 'view/dibujo.html',
+    controller: 'DibujoController'
+  })
+
+  .state('test', {
+    url: '/test',
+    templateUrl: 'view/test.html',
+    controller: 'DibujoController'
+  });;
+
+
+ // $routeProvider.otherwise({redirectTo: '/'});
+}]);
