651449e3b3fbdaaa46b24de4a3dff64d85a50f4d
Aracelly A <Aracelly A>
2016-12-21 17:01:07 -0300

    exp 1

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-var http = require('http')
+// var http = require('http')
 
-http.createServer(function (request, response){
-	response.writeHead(200, {"Content-Type": "text/plain"})
-	response.end("Hello World\n")
-}).listen(process.env.PORT)
+// http.createServer(function (request, response){
+// 	response.writeHead(200, {"Content-Type": "text/plain"})
+// 	response.end("Hello World\n")
+// }).listen(process.env.PORT)
+var express = require('express');
+var app = express();
+
+/* ==========================================================
+serve the static index.html from the public folder
+============================================================ */
+app.use(express.static(__dirname + '/'));
+
+app.listen(process.env.PORT, function() {
+  console.log('Server listening on port process.env.PORT');
+});
